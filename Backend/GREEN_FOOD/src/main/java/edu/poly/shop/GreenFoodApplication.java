f10fee7605e95a6889e043cdb1d8911150d02b49
VanHai2404 <hovanhai24042003@gmai.com>
2023-10-17 05:36:46 +0700

    Thêm dự án GREEN_FOOD

---
+++ Backend/GREEN_FOOD/src/main/java/edu/poly/shop/GreenFoodApplication.java
@@ -0,0 +1,13 @@
+package edu.poly.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenFoodApplication.class, args);
+	}
+
+}
